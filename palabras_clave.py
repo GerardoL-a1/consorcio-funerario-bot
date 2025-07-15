e4b21b391042dcca1c82fdf1279461b2c4768069
GerardoL-a1 <jerryvlogs086@gmail.com>
2025-07-14 22:21:53 -0600

    Delete palabras_clave.py

--- palabras_clave.py
+++
@@ -1,4 +0,0 @@
-claves_planes = ["plan", "planes", "servicio", "servicios", "paquete", "información", "informacion"]
-claves_emergencia = ["emergencia", "urgente", "fallecido", "murió", "murio", "accidente", "suceso", "acaba de fallecer", "acaba de morir"]
-claves_ubicacion = ["ubicación", "ubicaciones", "sucursal", "sucursales", "dirección", "direccion"]
-claves_cierre = ["gracias", "ok", "vale", "de acuerdo", "listo", "perfecto", "entendido", "muy bien"]
